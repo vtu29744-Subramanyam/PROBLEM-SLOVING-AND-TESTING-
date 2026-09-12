aa3204673e4d9ab6d599c51eb32e0d42d0e3a122
vtu29744-Subramanyam <vtu29744@veltech.edu.in>
2026-09-12 09:34:08 +0530

    Update Time Conversion week 4.java

--- PSTJ/Time Conversion week 4.java
+++ PSTJ/Time Conversion week 4.java
@@ -12,14 +12,6 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
-
-    /*
-     * Complete the 'timeConversion' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts STRING s as parameter.
-     */
-
     public static String timeConversion(String s) {
         String period = s.substring(8);
     int hour = Integer.parseInt(s.substring(0, 2));
